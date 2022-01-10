6a71c626740f963507dd018573c538a2f8c15c87
fadellh <fadel.lukman.dev@gmail.com>
2022-01-10 21:18:25 +0700

    feature: jwt configiration

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module root-auth
 
 go 1.13
 
-require github.com/labstack/echo/v4 v4.6.2 // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 
 func main() {
-	fmt.Println("hello world root")
+	e := echo.New()
+
+	//midleware
+	e.Use(middleware.Logger())
+
+	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey:  []byte("secret"),
+		TokenLookup: "query:token",
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// e.POST("/users", saveUser)
+	// e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: e,
+		//ReadTimeout: 30 * time.Second, // customize http.Server timeouts
+	}
+
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
